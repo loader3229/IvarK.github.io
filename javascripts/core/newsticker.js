3380cd8ceb577c099f6374acd2dcb8b712259665
Aarex Tiaokhiao <aarextiaokhiao@gmail.com>
2021-10-19 22:58:45 -0400

    Update newsticker.js

--- javascripts/core/newsticker.js
+++ javascripts/core/newsticker.js
@@ -497,6 +497,7 @@ amNewsArray = [
 ["Aarex went back to Googology, just to compete with strongest computable notations. Will he do it?", "tmp.ngp3", "am279"],
 ["Although the number on the only posted 9/18/2021 news ticker number is probably in the range of 120 <= x <= 600", true, "am280"],
 ["In an alternate universe, @TruthNotScam9 convinced Aarex to add Scamify, leaving many sparks of outrage from the players. Both of them died soon afterward.", "tmp.ngp3", "am281"],
+["It's 2025, Aarex became a googologist legend. He is not yet addicted to large number games...", "am282"],
 /*NEXT ID: am274*/
 ];}
 
